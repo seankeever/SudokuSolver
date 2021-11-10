5b5ae5003dca3668effd7db1bba2e69cf2e8f678
ravenlogistics\sean <sean@ravenlogistics.com>
2021-11-10 10:58:05 -0500

    Inital Project

---
+++ SudokuMaster/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace SudokuMaster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string puzzlePath = @"C:\Users\sean\source\repos\SudokuSolver\Puzzles\";
+            string puzzleName = "puzzle1.txt";
+
+            string fullPath = puzzlePath + puzzleName;
+            string puzzleContent = File.ReadAllText(fullPath);
+
+            Console.WriteLine(puzzleContent);
+        }
+    }
+}
